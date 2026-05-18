00be5c2bda23eaa836d63533acbf7d6b29014a28
kaedechi <112961927+kaedechi@users.noreply.github.com>
2026-05-19 00:31:49 +0300

    Update hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -7,22 +7,3 @@ hl.env("XCURSOR_THEME", "Quintom_Ink")
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("GTK_THEME", "Adwaita-dark")
-
------------------------
------ PERMISSIONS -----
------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Permissions/
--- Please note permission changes here require a Hyprland restart and are not applied on-the-fly
--- for security reasons
-
--- hl.config({
---   ecosystem = {
---     enforce_permissions = true,
---   },
--- })
-
--- hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
--- hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
--- hl.permission("/usr/(bin|local/bin)/hyprpm", "plugin", "allow")
-
