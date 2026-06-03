d4a21126aa553ad74874fa1536cee387e1a0296d
kaedechi <112961927+kaedechi@users.noreply.github.com>
2026-06-03 17:00:18 +0300

    Add files via upload

--- hypr/monitors.lua
+++ hypr/monitors.lua
@@ -4,3 +4,23 @@ hl.monitor({
     position = "0x0",
     scale    = "1.0",
 })
+hl.monitor({
+output = "HDMI-A-1",
+mode = "3840x2160@30",
+position = "auto",
+scale = "auto",
+})
+--hl.workspace_rule({
+  --  workspace = "8",
+    --monitor = "HDMI-A-1",
+--})
+--
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "6", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "7", monitor = "eDP-1" })
+
+hl.workspace_rule({ workspace = "8", monitor = "HDMI-A-1" })
